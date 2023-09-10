5c51d3a699fe75d0712f8212924fc04c5f845c6b
Marina Borges <mnascimentos11@gmail.com>
2023-09-10 10:37:42 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users do
-    resources :recipe, only: [ :index]
+    resources :recipe, only: [ :index ]
   end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
