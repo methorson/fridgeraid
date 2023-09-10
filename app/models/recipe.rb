96391a7517a3ead3c79cedc316f5491991cdbaae
Marina Borges <mnascimentos11@gmail.com>
2023-09-10 01:22:18 +0100

    Cloudinary config

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
 
   validates :name, :description, :instruction, :preparation_time, presence: true
   has_many :ingredients, through: :recipe_ingredients
+  has_one_attached :photo
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, :email, :password, presence: true
+  has_one_attached :photo
 end
