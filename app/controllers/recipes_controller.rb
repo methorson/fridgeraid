e299ef8f8110f8579ea64785189fdd74b122fab5
analopezrdz <annalucable@gmail.com>
2023-09-11 21:13:21 +0200

    nothing so far

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,49 +2,4 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
-
-  # def show
-  #   @recipe = Recipe.find(params[:id])
-  # end
-
-  # def new
-  #   @recipe = Recipe.new
-  # end
-
-  # def create
-  #   @recipe = Recipe.new(recipe_params)
-
-  #   if @recipe.save
-  #     redirect_to @recipe, notice: 'Recipe was successfully created.'
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  # def edit
-  #   @recipe = Recipe.find(params[:id])
-  # end
-
-  # def update
-  #   @recipe = Recipe.find(params[:id])
-
-  #   if @recipe.update(recipe_params)
-  #     redirect_to @recipe, notice: 'Recipe was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # def destroy
-  #   @recipe = Recipe.find(params[:id])
-  #   @recipe.destroy
-
-  #   redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
-  # end
-
-  private
-
-  def recipe_params
-    params.require(:recipe).permit(:name, :ingredients, :instructions)
-  end
 end
