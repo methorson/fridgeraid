55b97d5b0214c3cabe556dc44e769f73d812e30d
Maria Thorson <maria.thorson7@gmail.com>
2023-09-20 18:14:17 +0200

    fixed fridgeraid logo issue. Login button leads to fridge show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
      devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email photo password])
   end
 
+  def after_sign_in_path_for(resource)
+    fridge_list_path(:id)
+  end
+
 end
