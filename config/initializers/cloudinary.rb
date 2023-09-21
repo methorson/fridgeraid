8fc6eb42a5526f702f4ef6fa14e763c886e3ed0f
Marina Borges <mnascimentos11@gmail.com>
2023-09-21 09:56:34 +0100

    Error CLoudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,8 +1,6 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'dygpyg8qr'
-  config.api_key = ENV['CLOUDINARY_API_KEY']
-  config.api_secret = '***************************'
-  config.secure = true
-end
-
-
+# Cloudinary.config do |config|
+#   config.cloud_name = 'dygpyg8qr'
+#   config.api_key = ENV['CLOUDINARY_API_KEY']
+#   config.api_secret = '***************************'
+#   config.secure = true
+# end
