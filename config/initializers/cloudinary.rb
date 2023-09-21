9f1bb101477c84f63a13a81a22c6d2444cbda087
Maria Thorson <maria.thorson7@gmail.com>
2023-09-21 11:01:35 +0200

    css changes

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-# Cloudinary.config do |config|
-#   config.cloud_name = 'dygpyg8qr'
-#   config.api_key = ENV['CLOUDINARY_API_KEY']
-#   config.api_secret = '***************************'
-#   config.secure = true
-# end
