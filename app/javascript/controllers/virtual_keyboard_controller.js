3782646c3581c0007b28f484e0c91481836d9224
Marina Borges <mnascimentos11@gmail.com>
2023-09-29 19:05:52 +0100

    Search keyboard

--- app/javascript/controllers/virtual_keyboard_controller.js
+++ app/javascript/controllers/virtual_keyboard_controller.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 053354ac332785835a7e43ded3b495184af1ed26
 import { Controller } from "@hotwired/stimulus"
 import Keyboard from 'simple-keyboard';
 // import 'simple-keyboard/build/css/index.css';
@@ -21,6 +17,13 @@ export default class extends Controller {
     // console.log('test', this.keyboard);
   }
 
+  toggleKeyboard() {
+    console.log('Toggle keyboard called');
+    const keyboard = this.keyboardTarget;
+    console.log('Keyboard target:', keyboard);
+    keyboard.classList.remove('d-none');
+  }
+
   onChange(input) {
     console.log("search", this.searchTarget.value);
 
@@ -37,7 +40,3 @@ export default class extends Controller {
   //   console.log('Button pressed', button);
   // }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 053354ac332785835a7e43ded3b495184af1ed26
